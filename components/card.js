cb4f45655b12da7793be773f8450e43d59b0c163
azzyew <laisadnevescosta@gmail.com>
2021-03-08 21:41:43 -0300

    adding tailwind background

--- components/card.js
+++ components/card.js
@@ -4,33 +4,33 @@ export default function Card(props) {
     const card = props.card;
 
     return (
-        <div>
+        <div className="mt-5">
             <div className={`h-48 w-auto rounded-md flex ${card.bg}`}> {/* AQUI VAI A FOTO DE BACKGROUND! */}
-                <div className="bg-white bg-opacity-85 text-xs w-24 h-6 flex rounded-l-none rounded-t-none rounded-md">
-                    <span className={`pt-1 px-1 ml-1 ${card.categoryColor}`}>{card.category}</span>
+                <div className="bg-white bg-opacity-85 text-xs w-auto h-6 flex rounded-l-none rounded-t-none rounded-md">
+                    <span className={`pt-1 px-2 ml-1 ${card.categoryColor}`}>{card.category}</span>
                 </div>
 
-                <div className="bg-white bg-opacity-85 w-1/4 h-6 flex self-end justify-self-end rounded-3xl">
-                    <div className="text-sm px-1 self-end">
+                <div className="bg-white bg-opacity-85 w-1/4 h-6 flex self-end rounded-3xl flex-col">
+                    <div className="text-sm px-1 self-end flex-auto ">
                         <Image
                             src="/blueCam.svg"
                             alt="camera icon"
                             width={14.5}
                             height={11}
                         />
-                        <span className="text-bright pl-1">| </span>
-                        <span className="text-bright pl-1">{card.time}</span>
+                        <span className="text-bright pl-1 flex-auto">| </span>
+                        <span className="text-bright pl-1 flex-auto">{card.time}</span>
                     </div>
                 </div>
 
-                <div className="flex self-end">
+                <div className="flex absolute">
                     <div className="w-3/5 h-1.5 bg-pale"/>
                     <div className="w-2/5 h-1.5 bg-primary"/>
                 </div>
             </div>
             
             <div>
-                <h1 className="text-gray-800 text-sm pt-4 pb-2">Lorem ipsum dolor sit amet, consectetuer adipiscing e...</h1>
+                <h1 className="text-gray-900 text-sm pt-4 pb-1">Lorem ipsum dolor sit amet, consectetuer adipiscing e...</h1>
                 <div className="flex text-sm pb-2">
                     <h3 className="text-gray-500 pr-2">Produtos</h3>
                     <span className="text-gray-500 pr-2">| </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,27 @@ module.exports = {
       ...theme('colors'),
       'primary': '#197ACF',
       'secondary': '#1A8DE9',
+      'pale': '#96DCFA',
     }),
     textColor: theme => ({
       ...theme('colors'),
       'pale': '#96DCFA',
+      'bright': '#197ACF',
     }),
+    borderWidth: {
+      DEFAULT: '1px',
+      '0.5': '0.5px',
+    },
     extend: {
       backgroundImage: theme => ({
         'top-hero': "url('../public/bg.png')",
-      })
+        'hero1': "url('../public/bg1.svg')",
+        'hero2': "url('../public/bg2.svg')",
+        'hero3': "url('../public/bg3.svg')",
+      }),
+      backgroundOpacity: {
+        '85': '0.85',
+      }
     },
   },
   variants: {
