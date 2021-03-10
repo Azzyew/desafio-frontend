3ace7d8257743612a7e3e94314fd7447a02af31a
azzyew <laisadnevescosta@gmail.com>
2021-03-10 19:20:32 -0300

    menu style changes

--- components/menu.js
+++ components/menu.js
@@ -12,29 +12,29 @@ export default function Menu() {
                 />
             </div>
 
-            <div className="px-4 flex flex-col items-start">
+            <div className="px-6 flex flex-col items-start">
 
-                <button className="text-bright">Início</button>
-                <button>Minha Lista</button>
+                <button className="text-bright py-1">Início</button>
+                <button className="py-1">Minha Lista</button>
                 <hr className="text-gray-500 w-auto"/>
 
-                <span className="text-blue-800">NAVEGAR</span>
-                <button>Treinamentos</button>
-                <button>Trilhas</button>
-                <button>Podcasts</button>
+                <span className="text-blue-800 py-3">NAVEGAR</span>
+                <button className="py-1">Treinamentos</button>
+                <button className="py-1">Trilhas</button>
+                <button className="py-1">Podcasts</button>
                 <hr />
 
-                <span className="text-blue-800">CATEGORIAS</span>
-                <button>Ferramentas e Sistemas</button>
-                <button>Gestão Estratégica</button>
-                <button>Produtos</button>
-                <button className="text-bright ml-2 text-sm">Ver todas</button> {/* seta aqui! */}
+                <span className="text-blue-800 py-3">CATEGORIAS</span>
+                <button className="py-1">Ferramentas e Sistemas</button>
+                <button className="py-1">Gestão Estratégica</button>
+                <button className="py-1">Produtos</button>
+                <button className="text-bright ml-2 text-sm py-1">Ver todas</button> {/* seta aqui! */}
                 <hr />
 
-                <span className="text-blue-800">MATERIAL DE APOIO</span>
-                <button>Ferramentas</button>
-                <button>Biblioteca</button>
-                <button>Anotações</button>
+                <span className="text-blue-800 py-3">MATERIAL DE APOIO</span>
+                <button className="py-1">Ferramentas</button>
+                <button className="py-1">Biblioteca</button>
+                <button className="py-1">Anotações</button>
             </div>
         </div>
     )
